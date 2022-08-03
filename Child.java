76f42da75c120c98ccb1a80db22de4513d80abc6
gpganeshpatekar <gpganeshpatekar@gmail.com>
2022-08-03 16:25:26 +0530

    added parent and child class file

---
+++ Child.java
@@ -0,0 +1,8 @@
+public class Child extends Parent{
+
+	public static void main(String[] args){
+	
+	System.out.println("Child Class Main");
+
+	}
+}
---
+++ Parent.java
@@ -0,0 +1,8 @@
+public class Parent{
+
+	public static void main(String[] args){
+	
+	System.out.println("Parent Class Main");
+
+	}
+}
